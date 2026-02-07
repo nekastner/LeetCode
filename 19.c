9ca6467700e737b4cf5d3338bfa7953d34673666
Noah Elias Kastner <elias@kastner-home.com>
2026-02-07 18:36:24 +0100

    added solution for 19

---
+++ 19.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    // if list.length <= 1
+    if (head == NULL || head->next == NULL) return NULL;
+    // else
+    struct ListNode* toDeletePrevious = NULL; // node in front of <node to delete>
+    const struct ListNode* current = head; // current node on iteration
+    int list_length = 0;
+    while (current != NULL) {
+        list_length++; // increment list length counter
+        if (list_length == n+1) toDeletePrevious = head;
+        else if (list_length > n) toDeletePrevious = toDeletePrevious->next;
+        current = current->next; // jump into next node
+    }
+    // if first element shall be removed
+    if (list_length == n) {
+        struct ListNode* head_new = head->next; // save new head
+        head->next = NULL; // remove link to list in <node to delete>
+        return head_new;
+    }
+    // else
+    struct ListNode* toDelete = toDeletePrevious->next; // save <node to delete>
+    toDeletePrevious->next = toDelete->next; // remove link in list to <node to delete>
+    toDelete->next = NULL; // remove link to list in <node to delete>
+    return head;
+}
--- 67.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by user on 2026-02-07.
-//
-
-#ifndef LEETCODE_67_H
-#define LEETCODE_67_H
-
-#endif //LEETCODE_67_H
