944a6ff7d07ae7089d9c483579e2a405fdbd5254
Noah Elias Kastner <elias@kastner-home.com>
2026-02-07 18:26:50 +0100

    added solution for 67

---
+++ 67.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+char* addBinary(const char* a, const char* b) {
+    const size_t a_len = strlen(a);
+    const size_t b_len = strlen(b);
+    const size_t c_len = (a_len >= b_len ? a_len : b_len) + 1;
+    char* c = malloc(c_len+1);
+    c[c_len] = '\0';
+    unsigned char transfer = 0; // var to save transfer in calculation
+    for (int i = 0; i < c_len; i++) {
+        // calc indexes for a and b from behind
+        const int a_i = (int)a_len - 1 - i;
+        const int b_i = (int)b_len - 1 - i;
+        const int c_i = (int)c_len - 1 - i;
+        // get values
+        const unsigned char a_val = (a_i >= 0) ? a[a_i] - '0' : 0;
+        const unsigned char b_val = (b_i >= 0) ? b[b_i] - '0' : 0;
+        // calc
+        const unsigned char sum = a_val + b_val + transfer;
+        transfer = sum / 2;
+        c[c_i] = (char)((sum % 2) + '0');
+    }
+    // check start of actual value
+    int start_index = 0;
+    for (int i = 0; i < c_len - 1; i++) { // leave at least one 0, to not remove the whole result
+        if (c[i] != '0') break;
+        start_index++;
+    }
+    memmove(c, c + start_index, strlen(c + start_index) + 1);
+    // return result
+    return c;
+}
---
+++ 67.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 2026-02-07.
+//
+
+#ifndef LEETCODE_67_H
+#define LEETCODE_67_H
+
+#endif //LEETCODE_67_H
