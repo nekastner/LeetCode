7e68cde5b6d990cddfbc559967bd8dec253d29ab
Noah Elias Kastner <elias@kastner-home.com>
2026-02-09 14:56:57 +0100

    solved 26

--- 26.c
+++ 26.c
@@ -1,13 +1,27 @@
-int removeDuplicates(int* nums, int numsSize) {
-    int nums_filtered[numsSize];
-    nums_filtered[0] = nums[0];
-    int nums_filtered_i = 0;
+#include <string.h>
+
+int removeDuplicates(int* nums, const int numsSize) {
+
+    // nums.length == (0 || 1)
+    if (numsSize <= 1) return numsSize;
+
+    // prepare new array
+    int nums_unique[numsSize];
+    int nums_unique_index = 0;
+
+    // fill array with first value
+    nums_unique[nums_unique_index] = nums[0];
+    nums_unique_index++;
+
+    // scan numbers and save uniques
     for (int i = 1; i < numsSize; i++) {
-        if (nums[i] == nums_filtered[nums_filtered_i]) continue;
-        nums_filtered[++nums_filtered_i] = nums[i];
-        int duplicates_count = nums_filtered_i + 1;
-        while (nums_filtered_i < numsSize) {
-            nums_filtered[++nums_filtered_i] = -1;
-        }
-        return duplicates_count;
+        if (nums[i] == nums_unique[nums_unique_index - 1]) continue;
+        nums_unique[nums_unique_index] = nums[i];
+        nums_unique_index++;
     }
+
+    // overwrite nums with unique nums
+    memmove(nums, nums_unique, numsSize * sizeof(int));
+    // return amount of unique numbers
+    return nums_unique_index;
+}
