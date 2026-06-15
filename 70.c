82283d18f94b4be22c4f3bd4166b52bf77cc13f9
Noah Elias Kastner <elias@kastner-home.com>
2026-06-15 20:09:25 +0200

    solved 70 with help of ai (gemini) but understood the way it works and even further improved the algorithm

---
+++ 70.c
@@ -0,0 +1,25 @@
+int climbStairs(const int n) {
+
+    /* Walk the steps down from above
+     * and add the possibilities from the last two steps together
+     * to get the current possibilities
+     * until you reach the bottom.
+     * Because you can only come from one or two steps from above,
+     * and you just take their results with you.
+     */
+
+    // no calculation required for first two steps
+    if (n <= 2) {
+        return n;
+    }
+
+    // calculate result for current step from results of the last two steps
+    int results[3] = {1, 2, 0};
+    for (int i = 3; i <= n; i++) {
+        results[2] = results[0] + results[1];
+        results[0] = results[1];
+        results[1] = results[2];
+    }
+
+    return results[2];
+}
