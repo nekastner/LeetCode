ee6a318935f5ee82560584b0e1da09b076be66d8
Noah Elias Kastner <elias@kastner-home.com>
2026-06-15 18:19:09 +0200

    solved 61

---
+++ 61.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+
+    // if list is empty
+    if (head == NULL) {
+        return head;
+    }
+
+    // check length and make tail point to head (so list stays complete after shifting head)
+    int len = 1;
+    struct ListNode* tail = head;
+    while (tail->next != NULL) {
+        tail = tail->next;
+        len++;
+    }
+    k %= len;
+    tail->next = head;
+
+    // find new head and make new tail point to null
+    struct ListNode* new_tail = head;
+    for (int i = 0; i < len - k; i++) {
+
+        if (i >= 1) {
+            new_tail = new_tail->next;
+        }
+
+        head = head->next;
+    }
+    new_tail->next = NULL;
+
+    // return new head
+    return head;
+}
