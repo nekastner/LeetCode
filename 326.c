b27e15f5eedc9f86835328f8e995015055f94a8e
Noah Elias Kastner <elias@kastner-home.com>
2026-06-25 21:02:21 +0200

    imrpoved 326 by reducing it by one stack variable

--- 326.c
+++ 326.c
@@ -1,15 +1,14 @@
 #include <stdbool.h>
 
-bool isPowerOfThree(const int n) {
+bool isPowerOfThree(int n) {
 
     // handle edge case
     if (n == 0) return false;
 
     // check if n is power of 3
-    int n_rest = n;
     while (true) {
-        if (n_rest == 1) return true;
-        if (n_rest % 3 != 0) return false;
-        n_rest /= 3;
+        if (n == 1) return true;
+        if (n % 3 != 0) return false;
+        n /= 3;
     }
 }
