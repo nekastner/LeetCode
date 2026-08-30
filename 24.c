3307d9c2e7235526f4eec9c7c59e2047e258bab7
Noah Elias Kastner <elias@kastner-home.com>
2026-08-30 14:34:01 +0200

    solved 24

--- 24.c
+++ 24.c
@@ -8,19 +8,34 @@ struct ListNode
 
 static struct ListNode* swapPairs(struct ListNode* head)
 {
-    struct ListNode* indirect = head;
-
-    while (indirect != NULL && indirect->next != NULL)
+    // if there is no valid pair of nodes
+    if (head == NULL || head->next == NULL)
     {
-        // swap pair
-        struct ListNode* temp = indirect->next;
-        indirect->next = temp->next;
-        temp->next = indirect;
-        // iterate to next pair
-        indirect = indirect->next;
+        return head;
     }
 
-    return head;
-}
+    // create head mock so there is a pre pair node for the first pair
+    struct ListNode head_mock;
+    head_mock.val = -1;
+    head_mock.next = head;
 
-// TODO: solve (pairs do not point to each other properly anymore, is in-memory swap okay?)
+    // start iterating over node pairs
+    struct ListNode* pre_pair_node = &head_mock;
+    while (pre_pair_node != NULL && pre_pair_node->next != NULL && pre_pair_node->next->next != NULL)
+    {
+        // buffer addresses of both pair nodes
+        struct ListNode* pair_node_1 = pre_pair_node->next;
+        struct ListNode* pair_node_2 = pair_node_1->next;
+
+        // swap pair nodes via pointer changes
+        pre_pair_node->next = pair_node_2;
+        pair_node_1->next = pair_node_2->next;
+        pair_node_2->next = pair_node_1;
+
+        // iterate to node before next pair
+        pre_pair_node = pair_node_1; // pair node 1 is now pair node 2 (after swap)
+    }
+
+    // return next of head mock (actual new head)
+    return head_mock.next;
+}
