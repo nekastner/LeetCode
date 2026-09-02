c1ed610cfc688f8f70fb192a3acc24909f020a54
Noah Elias Kastner <elias@kastner-home.com>
2026-09-02 14:45:30 +0200

    reviewed 70

--- 70.c
+++ 70.c
@@ -1,5 +1,3 @@
-// TODO: review
-
 int climbStairs(const int n) {
 
     /* Walk the steps down from above
@@ -16,9 +14,11 @@ int climbStairs(const int n) {
     }
 
     // calculate result for current step from results of the last two steps
-    int results[3] = {1, 2, 0}; // pre-previous step, previous step, current step
+    int results[3] = {1, 2, 0}; // previous-previous step, previous step, current step
     for (int i = 3; i <= n; i++) {
+        // calculate possibilities of current step
         results[2] = results[0] + results[1];
+        // shift possibilities for the last two steps (because we go further and the last two steps are not the same anymore)
         results[0] = results[1];
         results[1] = results[2];
     }
