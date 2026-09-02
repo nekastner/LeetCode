5ddfa018c09fc0199870578c9ca95fbcd86b5f3c
Noah Elias Kastner <elias@kastner-home.com>
2026-09-02 13:59:29 +0200

    reviewd 26 (memory inefficient, but runtime efficient)

--- 26.c
+++ 26.c
@@ -1,29 +1,28 @@
 #include <string.h>
 
-// TODO: review
+// annotation: memory inefficient, but runtime efficient (each number needs to get touched less often)
 
 int removeDuplicates(int* nums, const int numsSize) {
 
-    // nums.length == (0 || 1)
+    // if the array length is less than 2, duplicates are impossible
     if (numsSize <= 1) return numsSize;
 
     // prepare new array
     int nums_unique[numsSize];
     int nums_unique_index = 0;
 
-    // fill array with first value
-    nums_unique[nums_unique_index] = nums[0];
-    nums_unique_index++;
+    // fill array with first value (required for checking for duplicates in loop)
+    nums_unique[nums_unique_index++] = nums[0];
 
     // scan numbers and save uniques
     for (int i = 1; i < numsSize; i++) {
         if (nums[i] == nums_unique[nums_unique_index - 1]) continue;
-        nums_unique[nums_unique_index] = nums[i];
-        nums_unique_index++;
+        nums_unique[nums_unique_index++] = nums[i];
     }
 
-    // overwrite nums with unique nums
+    // overwrite nums array with unique nums array
     memmove(nums, nums_unique, numsSize * sizeof(int));
+
     // return amount of unique numbers
     return nums_unique_index;
 }
